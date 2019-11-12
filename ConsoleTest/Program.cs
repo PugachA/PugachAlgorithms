ff91e62f96508a5b1e715349244e351f5899245b
PugachA <33941018+PugachA@users.noreply.github.com>
2019-11-12 13:11:17 +0300

    Добавьте файлы проекта.

---
+++ ConsoleTest/Program.cs
@@ -0,0 +1,35 @@
+﻿using PugachAlgorithms;
+using System;
+using System.Diagnostics;
+
+namespace ConsoleTest
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            const int n = 1000;
+            int[] a = new int[n];
+            Random rnd = new Random();
+            for (int i = 0; i < n; i++)
+                a[i] = rnd.Next() % 500;
+
+            Timing timing = new Timing();
+            Stopwatch stpWatch = new Stopwatch();
+
+            timing.StartTime();
+            stpWatch.Start();
+            for(int i = 0; i < 500000; i++)
+            {
+                rnd.Next();
+            }
+
+            stpWatch.Stop();
+            timing.StopTime();
+
+            Console.WriteLine("StopWatch: " + stpWatch.Elapsed.ToString());
+            Console.WriteLine("Timing: " + timing.Result().ToString());
+            Console.ReadLine();
+        }
+    }
+}
---
+++ PugachAlgorithms/Timing.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Diagnostics;
+
+namespace PugachAlgorithms
+{
+    public class Timing
+    {
+        TimeSpan duration;
+        TimeSpan[] threads;
+
+        public Timing()
+        {
+            duration = new TimeSpan(0);
+            threads = new TimeSpan[Process.GetCurrentProcess().Threads.Count];
+        }
+
+        public void StartTime()
+        {
+            GC.Collect();
+            GC.WaitForPendingFinalizers();
+            for (int i = 0; i < threads.Length; i++)
+                threads[i] = Process.GetCurrentProcess().Threads[i].UserProcessorTime;
+        }
+
+        public void StopTime()
+        {
+            TimeSpan tmp;
+            for (int i = 0; i < threads.Length; i++)
+            {
+                tmp = Process.GetCurrentProcess().Threads[i].UserProcessorTime.Subtract(threads[i]);
+                if (tmp > TimeSpan.Zero)
+                    duration = tmp;
+            }
+        }
+
+        public TimeSpan Result()
+        {
+            return duration;
+        }
+    }
+}
